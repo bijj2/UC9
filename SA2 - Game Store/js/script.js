54a43e7c0400284911de50ad81b0b6e20f011481
bijj2 <bcorrea@mtsolucoes.com.br>
2022-04-25 10:57:39 -0300

    inclusão de javascript aula 3

---
+++ SA2 - Game Store/js/script.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){
+
+    $("#barras").click(function(){
+        $("#menu").toggleClass("menu-ativo")
+        })
+    })
+
+$(document).ready(function() {
+    $("#carrossel img:eq(0)").addClass("banner-ativo").show()
+
+})
+
+setInterval(slide,1000);
+
+function slide(){
+    if($(".banner-ativo").next().length){
+        $(".banner-ativo").removeClass("banner-ativo").hide().next().addClass("banner-ativo").show()
+    }else{
+        $(".banner-ativo").removeClass().hide()
+        $("#carrossel img:eq(0)").addClass("banner-ativo").show()
+    }
+}
+
+//function mostrarpopup(){
+//    window.alert("Seja Bem-Vindo")
+//}
+
+let listaDestaques = [
+    {
+        Nome:"teste teste teste Bia",
+        Preco:"De:3.999,00",
+        Preçopromo: "Por:1.500,55",
+        imagem:"src=img/Produto 2.png" ,
+    },
+    {
+        Nome:"teste teste teste Bia",
+        Preco:"De:3.999,00",
+        Preçopromo: "Por:1.500,55",
+        imagem:"src=img/Produto 2.png" ,
+    },
+    {
+        Nome:"teste teste teste Bia",
+        Preco:"De:3.999,00",
+        Preçopromo: "Por:1.500,55",
+        imagem:"src=img/Produto 2.png" ,
+    },
+    {
+        Nome:"teste teste teste Bia",
+        Preco:"De:3.999,00",
+        Preçopromo: "Por:1.500,55",
+        imagem:"src=img/Produto 2.png" ,
+    },
+    {
+        Nome:"teste teste teste Bia",
+        Preco:"De:3.999,00",
+        Preçopromo: "Por:1.500,55",
+        imagem:"src=img/Produto 2.png" ,
+    },
+    {
+        Nome:"teste teste teste Bia",
+        Preco:"De:3.999,00",
+        Preçopromo: "Por:1.500,55",
+        imagem:"src=img/Produto 2.png" ,
+    },
+    {
+        Nome:"teste teste teste Bia",
+        Preco:"De:3.999,00",
+        Preçopromo: "Por:1.500,55",
+        imagem:"src=img/Produto 2.png" ,
+    },
+    {
+        Nome:"teste teste teste Bia",
+        Preco:"De:3.999,00",
+        Preçopromo: "Por:1.500,55",
+    },
+]
+
+function carregarDestaques(){
+    let espaco = document.getElementById("destaques")
+    
+    let template = "";
+
+    for  (let index = 0; index < listaDestaques.length; index++) {
+        const destaques = listaDestaques[index]; 
+        
+        template += ` <div class="Produto">
+        <a href=""><img class="imagem" src="img/Produto 1.png"></a>
+        <h4>${destaques.Nome}</h4>
+        <p>DE: R$ 500,00</p>
+        <p>Por:459,99</p>
+        <a href=""><img class="comprar" src="img/botao-comprar.png"></a>
+        </div>`
+    }
+
+    espaco.innerHTML = template; 
+    }
